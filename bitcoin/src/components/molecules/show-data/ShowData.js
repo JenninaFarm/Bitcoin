9d8ac8c98fc9a3e8c3d34b3b154702842208a06a
Jennina Färm <jennina.farm@exove.fi>
2021-11-19 23:29:26 +0200

    bitcoin-decrease: fetch data from the CoinCecko

--- bitcoin/src/components/molecules/show-data/ShowData.js
+++ bitcoin/src/components/molecules/show-data/ShowData.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 
-const ShowData = (props) => {
-  const [data, setData] = React.useState();
+const ShowData = ({dateFrom, dateTo}) => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    console.log(dateFrom);
+    console.log(dateTo);
+    console.log(data);
+    if( dateFrom !== undefined && dateTo !== undefined) {
+      fetchData();
+    }
+  }, [dateTo]);
 
   const fetchData = () => {
-    axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=1577836800&to=1609376400`)
+    axios.get(`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=eur&from=${dateFrom}&to=${dateTo}`)
       .then(res => {
         setData(res.data);
         console.log(res.data);
       });
   }
 
-  React.useEffect(() => {
-    if( data === undefined) {
-      fetchData();
-    }
-  }, [data]);
+  
 
   return (
     <div>
