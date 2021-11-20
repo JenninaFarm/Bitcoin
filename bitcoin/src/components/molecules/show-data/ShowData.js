85e5ef52159f463f219e2d41cbd1e7e6e3c1fb15
Jennina Färm <jennina.farm@exove.fi>
2021-11-20 11:38:49 +0200

    bitcoin-decrease: count granularity

--- bitcoin/src/components/molecules/show-data/ShowData.js
+++ bitcoin/src/components/molecules/show-data/ShowData.js
@@ -19,7 +19,8 @@ const ShowData = ({dateFrom, dateTo}) => {
     // count the day range
     // hox! remember to include the change in granularity
     const daysBetween = countDaysBetween(dateFrom, dateTo);
-    console.log('days between: ' + daysBetween);
+    const granularity = countGranularity(daysBetween);
+    console.log(granularity);
     // get the midnight prices 
     const dayPriceArray = getPricesAtMidnight(data.prices);
     // count the amount of decreasing days
@@ -34,6 +35,19 @@ const ShowData = ({dateFrom, dateTo}) => {
     const diffInMs = (to - from) * 1000;
     const oneDay = 1000 * 60 * 60 * 24;
     return Math.round(diffInMs / oneDay);
+  }
+//   1 day from query time = 5 minute interval data
+// 1 - 90 days from query time = hourly data
+// above 90 days from query time = daily data (00:00 UTC)
+
+  const countGranularity = (dayRange) => {
+    if (dayRange === 1) {
+      return 24 * 60 / 5 - 1;
+    } else if (dayRange > 1 && dayRange <= 90) {
+      return 24 - 1;
+    } else {
+      return 0;
+    }
   }
 
   const getPricesAtMidnight = (prices) => {
